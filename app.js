a3cf92887de81d52449c7808ff6444ffb44c49d3
claudiasgithub2 <138537469+claudiasgithub2@users.noreply.github.com>
2023-08-03 21:04:42 +0000

    formulario pronto con alerta ok

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ window.onload = function validarFormulario(event) {
 
     let envio = document.getElementById("enviar")
     envio.addEventListener("click", chequeo)
+    // ocultar();
+    document.getElementById('alerta').style.display = 'none';
 
     function chequeo() {
+        document.getElementById('alerta').style.display = 'none';
+
         //chequeo de la tarjeta
         let tarjeta = document.getElementById("tarjeta").value
         if (tarjeta == "") {
-            console.log("tarjetavacia")
             document.getElementById("tarjeta").className = "form-control bg-danger bg-opacity-50"
+            document.getElementById('alerta').style.display = 'flex';
+
         }
         else {
             document.getElementById("tarjeta").className = "form-control"
@@ -18,8 +23,9 @@ window.onload = function validarFormulario(event) {
         //chequeo CVC
         let cvc = document.getElementById("cvc").value
         if (cvc == "") {
-            console.log("cvcvacia")
             document.getElementById("cvc").className = "form-control bg-danger bg-opacity-50"
+            document.getElementById('alerta').style.display = 'flex';
+
         }
         else {
             document.getElementById("cvc").className = "form-control"
@@ -29,8 +35,9 @@ window.onload = function validarFormulario(event) {
         //chequeo Amount
         let amount = document.getElementById("amount").value
         if (amount == "") {
-            console.log("amountvacia")
             document.getElementById("amount").className = "form-control bg-danger bg-opacity-50"
+            document.getElementById('alerta').style.display = 'flex';
+
         }
         else {
             document.getElementById("cvc").className = "form-control"
@@ -39,60 +46,57 @@ window.onload = function validarFormulario(event) {
         //chequeo FirstName
         let firstname = document.getElementById("firstname").value
         if (firstname == "") {
-            console.log("firstnamevacia")
             document.getElementById("firstname").className = "form-control bg-danger bg-opacity-50"
+            document.getElementById('alerta').style.display = 'flex';
+
         }
         //chequeo LastName
         let lastname = document.getElementById("lastname").value
         if (lastname == "") {
-            console.log("lastnamevacia")
             document.getElementById("lastname").className = "form-control bg-danger bg-opacity-50"
+            document.getElementById('alerta').style.display = 'flex';
+
         }
         //chequeo city
         let city = document.getElementById("city").value
         if (city == "Choose...") {
-            console.log("cityvacia")
             document.getElementById("city").className = "form-control bg-danger bg-opacity-50"
+            document.getElementById('alerta').style.display = 'flex';
+
         }
         //chequeo state
         let state = document.getElementById("state").value
         if (state == "") {
-            console.log("state")
             document.getElementById("state").className = "form-control bg-danger bg-opacity-50"
+            document.getElementById('alerta').style.display = 'flex';
+
         }
 
         //chequeo postalcode
         let postalcode = document.getElementById("postalcode").value
         if (postalcode == "") {
-            console.log("postalcode")
             document.getElementById("postalcode").className = "form-control bg-danger bg-opacity-50"
+            document.getElementById('alerta').style.display = 'flex';
+
         }
 
         //chequeo mensaje
         let mensajetext = document.getElementById("mensajetext").value
         if (mensajetext == "") {
-            console.log("mensajetext")
             document.getElementById("mensajetext").className = "form-control bg-danger bg-opacity-50"
+            document.getElementById('alerta').style.display = 'flex';
+
         }
         else {
             document.getElementById("mensajetext").className = "form-control"
 
         }
+       
     }
-    function ocultar(){
-   document.getElementById('alerta').style.display = 'none';
-     if (tarjeta !="" && cvc != "" && amount !="" && firstname !="" && lastname !="" && city !="" && state !="" && postalcode !="" && mensajetext) {
-          
-     }
-  }
+    
+
+
 
 }
 
-//ocultar alerta
 
-// function ocultar(){
-//    document.getElementById('alerta').style.display = 'none';
-//      if (tarjeta !="" && cvc != "" && amount !="" && firstname !="" && lastname !="" && city !="" && state !="" && postalcode !="" && mensajetext) {
-          
-//      }
-//   }
